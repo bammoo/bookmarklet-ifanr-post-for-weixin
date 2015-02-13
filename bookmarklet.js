29ca734f71f0ceb94caae9fc4421cc2453c68cdc
john <johnxiao@ifanr.com>
2015-02-13 16:03:36 +0800

    upd js

--- bookmarklet.js
+++ bookmarklet.js
@@ -1,25 +1,17 @@
-// javascript: void(
-(function() {
-  var d = document;
-  var w = window;
-  var f = 'http://mindstore.io/mind/create/';
-  /*var f='http://192.168.1.57:8080/lime/mind/create/';*/
-  var l = d.location;
-  var e = encodeURIComponent;
-  var p = '?mind_title=' + d.title;
-  var u = '&mind_url=' + e(l.href);
-  var g = f + p + u;
+javascript: void(0);
+(function($) {
+  var version = '版本：v.0.0.2。 update at：2015-7-23';
 
-  function a() {
-    if (!w.open(g, 't', 'toolbar=0,resizable=0,scrollbars=1,status=1,width=700,height=500')) {
-      l.href = g;
-    }
-  }
-  if (/Firefox/.test(navigator.userAgent)) {
-    setTimeout(a, 0);
-  } else {
-    a();
-  }
-  void(0);
-})()
-// );
+  // 去背景
+  $('html, body, #container, #container-inner, #content-outer, #content').css('background-color', 'transparent');
+
+  // 设置字体
+  $('article p, article div').css('font-size', '16px');
+  $('article p, article h3, article h2, article h1').css('padding', '0 15px');
+  $('.entry-content').find('span[id^="more"]').remove();
+  $('.entry-content').find('h1, h2 ,h3').css('font-weight', '700');
+  $('.entry-content').find('img').css('max-width', 'inherit');
+
+  alert('已完成！' + version);
+  console.log('已完成！' + version)
+})(jQuery)
